452d71a8a1c9d0d4286be4e48aac0f487b44803c
Brenda Garcia <brendagar88@gmail.com>
2021-02-08 11:29:19 -0600

    Enruta las nuevas páginas

--- src/configFirebase/configFirebase.js
+++ src/configFirebase/configFirebase.js
@@ -1,11 +1,12 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyDSXyBZP3YkLdhvWmQCcR2Od7vosEaJzLk",
-    authDomain: "chop-909dd.firebaseapp.com",
-    projectId: "chop-909dd",
-    storageBucket: "chop-909dd.appspot.com",
-    messagingSenderId: "94023060896",
-    appId: "1:94023060896:web:a07663bc975496c1ba28c5",
-    measurementId: "G-GMBBXN38TD"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyABUHXIMjqNVdKs_41stoDoM4Yb9XjFE4A",
+  authDomain: "socialnetworkchop.firebaseapp.com",
+  projectId: "socialnetworkchop",
+  storageBucket: "socialnetworkchop.appspot.com",
+  messagingSenderId: "1019869889548",
+  appId: "1:1019869889548:web:8cdd1ee97a661bba1e1a29",
+  measurementId: "G-RL2SD3837Y"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
--- src/main.js
+++ src/main.js
@@ -1,11 +1,32 @@
 //import { } from './lib/index.js';
-import { homeWelcome } from './homeWelcome.js';
+import { logIn } from './logIn.js';
+import { signUp } from './signUp.js';
+import { home } from './home.js';
+import { error404 } from './error404.js';
+//import {routes} from './routes.js';
 
+const rootDiv = document.getElementById('root');
 //  Muestra, imprime o renderiza el componente de la primera página cuando ésta carga
-const render = async () => {
-    const homeViewHTML = document.getElementById('root');
-    const view = homeWelcome();
-    homeViewHTML.innerHTML = await (view);
+const render =  async () => {
+    let viewWelcome = logIn();
+    rootDiv.innerHTML = await (viewWelcome);
+    router();
 };
 
+//Renderiza las páginas de acuerdo al hash de cada página
+const routes = {
+    '#/signup/': signUp,
+    '#/': home,
+}
+
+function router() {
+    let route = location.hash;
+    if (Object.keys(routes).includes(route)) {
+        rootDiv.innerHTML = routes[route];
+    } else if (route != "") {
+        rootDiv.innerHTML = error404;
+    }
+}
+
 window.addEventListener('load', render);
+window.addEventListener('hashchange', render);
