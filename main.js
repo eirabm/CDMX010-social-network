b3a51de0c232a32cda2b02113132f19a65aaa603
Eira Mazon <eira.b.mazon@gmail.com>
2021-02-20 15:13:12 -0600

    feat(authScript): functions for logIn with gitHub, Google and Facebook created

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 // Este es el punto de entrada de tu aplicacion
 
 import { myFunction } from './lib/index.js';
+import { signUp, logIn } from './lib/authPages.js';
+import { authGitHub, authGoogle, authFacebook } from './lib/authScript.js';
+
+const mainPage = document.getElementById('root');
+
+const routes = {
+  '#/signup/': signUp,
+  '#/login/': logIn,
+  '#/': home,
+};
 
 myFunction();
