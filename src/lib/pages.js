9fc5a3eca5d082654ddb4496fc40f5f1876b934e
LinnetteVazquez <lin23newcvaz@gmail.com>
2021-02-22 11:47:15 -0600

    refactor(pages):Add style mobile and desktop

--- src/lib/pages.js
+++ src/lib/pages.js
@@ -1,60 +1,7 @@
 export const homePage = `
-<header> 
-  <img src="https://user-images.githubusercontent.com/66543426/108579517-c7828480-72ec-11eb-8ac5-113ca9a42b76.png">
-  </>
- <img src="" alt=""/>
- <div class="tittle">
-  <h1>¡Aprende, cocina y comparte!</h1>
-  <p> Únete a nuestra comunidad para compartir tus mejores recetas</>
-  </div>
- <nav class="nav-header">
-  <label for="toggle" class="toggle-btn">
-      <span></span>
-      <span></span>
-      <span></span>    
-     </label> 
-      <input type="checkbox" id="toggle"/>
-        
-        <div class="nav-info">
-        <ul>
-          <li><p>Perfil</p></li>
-          <li><p>Sale</p></li>
-          <li><p>Sucursales</p></li>
-          <li><p>Salir</p></li>
-        </ul>
-        </div>
- </nav>  
-</header>
-<section>
-<article class="information-people">
-    <figure>
-      <img id ="perfil-desktop" src="https://www.softzone.es/app/uploads/2018/04/guest.png" alt="">
-    </figure>
-    <div class="people-datos">
-      <h2 id ="name"></h2>
-      <p id ="email"></p>
-    </div>
-    <ul class= "home-main-profile_options">
-      <li><a href= "#/profile/">Mi perfil</a></li>
-      <li><a href = "#/posts/">Mis post</a></li>
-      <li><a href= "#/favorites/">Mis likes</a></li>
-      <li class="signOut">Salir</li>
-    </ul>
-  </article>
-
-
-
-  <nav class = "nav-footer">
-    <img class="search" src="https://user-images.githubusercontent.com/66543426/108606335-38727c80-737f-11eb-8b4e-c358ee4ed4ee.png" alt="">
-    <img class="home" src="https://user-images.githubusercontent.com/66543426/108606367-5b049580-737f-11eb-87c6-7b7d357684af.png" alt="">
-    <img class="add" src="https://user-images.githubusercontent.com/66543426/108606384-740d4680-737f-11eb-9ea5-499edfc1316a.png" alt="">
-  </nav>
-`;
-
-export const perfilpage = `
 <header>
     <img class="chop"
-      src="https://user-images.githubusercontent.com/75234502/106777987-1d77dc80-660b-11eb-8d01-191b02855d50.png"
+      src="https://user-images.githubusercontent.com/66543426/108579517-c7828480-72ec-11eb-8ac5-113ca9a42b76.png"
       alt="">
     <div class ="tittle">
       <h1>¡Aprende, cocina y comparte!</h1>
@@ -96,4 +43,8 @@ export const perfilpage = `
   <h2>post</h2>
   </article>
   </section>
+`;
+
+export const perfilpage = `
+
 `;
