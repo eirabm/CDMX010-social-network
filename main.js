c7e971ab49dcf1a1f423a95c0433012cd49d37d5
Eira Mazon <eira.b.mazon@gmail.com>
2021-03-02 21:35:55 -0600

    feat(posts): posts created with user img and name

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 // Este es el punto de entrada de tu aplicacion
-import { authPage } from './lib/authPages.js';
-import { profilePage, newRecipePage, postsPage } from './lib/pages.js';
-import { createNewPost, previewIMG, getPosts } from './posts.js';
-import {
-  authSN, signUp, hasUserAuth, logInEmailPass, logOut, getData,
-} from './lib/authScript.js';
+import { authPage, logInDOM } from './lib/authPages.js';
+import { profilePage, newRecipePage, postsPage, mainPageContainer } from './lib/pages.js';
+import { newPost, previewIMG, getPosts } from './posts.js';
+import { authFunctions, hasUserAuth, logOut, getData } from './lib/authScript.js';
 
 // page
 const mainPage = document.getElementById('root');
-
 // function que limpia la url
 const clearPathname = (hash) => hash.replace('#/', '');
 
@@ -45,3 +42,56 @@ function logOutApp() {
   const btnSalir = document.getElementById('salir');
   btnSalir.addEventListener('click', logOut);
 }
+
+const routes = {
+  home: () => {
+    getPosts();
+    mainPage.innerHTML = mainPageContainer;
+    const pageContainer = document.getElementById('pageContainer');
+    pageContainer.innerHTML = postsPage;
+    salir();
+    getData((user) => {
+      const userPhoto = document.getElementById('foto');
+      const photo = user.photoURL;
+      console.log(photo);
+      userPhoto.src = photo;
+    });
+  },
+  
+  login: () => {
+    mainPage.innerHTML = authPage;
+    logInDOM();
+    authFunctions();
+  },
+
+  perfil: () => {
+    mainPage.innerHTML = mainPageContainer;
+    const pageContainer = document.getElementById('pageContainer');
+    pageContainer.innerHTML = profilePage;
+    getData((user) => {
+      const userPhoto = document.getElementById('fotos');
+      console.log(userPhoto);
+      const userName = document.getElementById('nombre');
+      console.log(userName);
+      const userEmail = document.getElementById('correo');
+      console.log(userEmail);
+      const photo = user.photoURL;
+      console.log(photo);
+      const name = user.displayName;
+      console.log(name);
+      const email = user.email;
+      console.log(email);
+      userPhoto.src = photo;
+      userName.innerHTML = name;
+      userEmail.innerHTML = email;
+    });
+  },
+
+  new: () => {
+    mainPage.innerHTML = mainPageContainer;
+    const pageContainer = document.getElementById('pageContainer');
+    pageContainer.innerHTML = newRecipePage;
+    previewIMG();
+    newPost();
+  },
+};
