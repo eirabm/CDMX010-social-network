e03ea955118ae1eab2ebcc121539d10b12226f74
Eira Mazon <eira.b.mazon@gmail.com>
2021-03-07 20:54:30 -0600

    refactor(posts): edit function style and selectors finished, color red on like

---
+++ src/lib/postFunctions.js
@@ -0,0 +1,30 @@
+export const editPost = (id) => {
+  const recipes = document.getElementById('post-container');
+  const post = recipes.querySelector(`[data-id=${id}]`);
+
+  const editedTitle = post.querySelector('[id=recipeTitle]');
+  const editedDescription = post.querySelector('[id=recipeDescription]');
+  post.querySelector('.recipe-template .edit-buttons').style.display = 'block';
+
+  editedTitle.contentEditable = true; editedTitle.style.border = '1.5px solid black';
+  editedDescription.removeAttribute('readonly'); editedDescription.style.border = '1.5px solid black';
+
+  const saveNewDataBtn = post.querySelector('[id=saveChanges]');
+  saveNewDataBtn.addEventListener('click', () => {
+    const editedTitleValue = document.getElementsByTagName('h3')[0].innerHTML;
+    const editedDescriptionValue = editedDescription.value;
+
+    firebase.firestore().collection('post').doc(id)
+      .update({
+        title: editedTitleValue,
+        post: editedDescriptionValue,
+      });
+  });
+
+  const cancel = post.querySelector('[id=cancelChanges]');
+  cancel.addEventListener('click', () => {
+    editedTitle.contentEditable = false; editedTitle.style.border = 'none';
+    editedDescription.readOnly = true; editedDescription.style.border = 'none';
+    post.querySelector('.recipe-template .edit-buttons').style.display = 'none';
+  });
+};
